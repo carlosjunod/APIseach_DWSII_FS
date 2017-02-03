b643ab2be31214bfd025a0120699a7b5649b27b4
Carlos Junod <carlos@junod.cl>
2017-02-02 22:03:17 -0500

    adding styles to search result: background gradient, font color

--- js/search.js
+++ js/search.js
@@ -1,30 +1,39 @@
 var container = document.querySelector(".elements-list");
 var seachForm = document.querySelector('form');
+var msgSearch = document.querySelector('h3.message');
 
 
 function populateList(){
     let searchValue = document.querySelector("#field").value;
     let searchUrl = `http://www.omdbapi.com/?s=${searchValue}`;
+
     fetch (searchUrl)
     .then((response) => {
         return response.json();
     }).then( (m) => {
         let movies = m.Search;
-        console.log(movies.Search);
+        console.log(movies);
+        //console.log(movies.Search);
 
         // iterating for each value
         movies.forEach((movie) => {
             // console.log(movie.Title);
-            var movieElement = `<li>${movie.Title}<img src="${movie.Poster}"</li>`;
+            var movieElement = `<li style="background-image:url('${movie.Poster}')">
+
+            <h2>${movie.Title} <span>${movie.Year}</span></h2>
+            </li>`;
             // console.log(movieElement);
             container.insertAdjacentHTML('beforeend', movieElement);
         })
+
+        msgSearch.innerHTML = `Showing results for: <strong>${searchValue}</strong>`
     })
 }
 
 function search(event){
-    container.innerHTML = '';
     event.preventDefault();
+    container.innerHTML = '';
+    msgSearch.innerHTML = '';
     populateList();
 }
 
