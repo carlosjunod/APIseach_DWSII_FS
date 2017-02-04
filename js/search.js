51d93711b20e6fb63f2fafaf8cea6215385bd431
Carlos Junod <carlos@junod.cl>
2017-02-03 19:04:52 -0500

    Changing API to themoviedb.org

--- js/search.js
+++ js/search.js
@@ -5,29 +5,34 @@ var msgSearch = document.querySelector('h3.message');
 
 function populateList(){
     let searchValue = document.querySelector("#field").value;
-    let searchUrl = `http://www.omdbapi.com/?s=${searchValue}`;
+    let searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=82ba59d3ee5b386cb7d3b0a2adda70a7&language=en-US&query=${searchValue}&page=1&include_adult=false`;
+    console.log(searchUrl);
 
     fetch (searchUrl)
     .then((response) => {
         return response.json();
     }).then( (m) => {
-        let movies = m.Search;
+        let movies = m.results;
         console.log(movies);
         //console.log(movies.Search);
 
         // iterating for each value
         movies.forEach((movie) => {
             // console.log(movie.Title);
-            var movieElement = `<li style="background-image:url('${movie.Poster}')">
 
-            <h2>${movie.Title} <span>${movie.Year}</span></h2>
+            var relase = new Date(movie.release_date).getFullYear();
+
+            var movieElement = `<li style="background-image:url('https://image.tmdb.org/t/p/w500/${movie.poster_path}')">
+            <h2>${movie.title} <span>${relase}</span></h2>
             </li>`;
             // console.log(movieElement);
             container.insertAdjacentHTML('beforeend', movieElement);
         })
 
         msgSearch.innerHTML = `Showing results for: <strong>${searchValue}</strong>`
-    })
+    }).catch(function(error) {
+        msgSearch.innerHTML = `ERROR: <strong>${searchValue}</strong> <br/> ${error.message}`
+});
 }
 
 function search(event){
