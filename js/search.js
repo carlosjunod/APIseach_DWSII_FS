81686dde0ac2d1fc2201ebdc35767a7cc45bcd7d
Carlos Junod <carlos@junod.cl>
2017-02-03 19:46:42 -0500

    removing adult movies from the search results

--- js/search.js
+++ js/search.js
@@ -13,23 +13,28 @@ function populateList(){
         return response.json();
     }).then( (m) => {
         let movies = m.results;
-        console.log(movies);
-        //console.log(movies.Search);
 
-        // iterating for each value
-        movies.forEach((movie) => {
-            // console.log(movie.Title);
+        if (movies.length > 0) {
+            // iterating for each value
+            movies.forEach((movie) => {
+                // console.log(movie.Title);
 
-            var relase = new Date(movie.release_date).getFullYear();
+                var relase = new Date(movie.release_date).getFullYear();
 
-            var movieElement = `<li style="background-image:url('https://image.tmdb.org/t/p/w500/${movie.poster_path}')">
-            <h2>${movie.title} <span>${relase}</span></h2>
-            </li>`;
-            // console.log(movieElement);
-            container.insertAdjacentHTML('beforeend', movieElement);
-        })
+                var movieElement = `<li style="background-image:url('https://image.tmdb.org/t/p/w500/${movie.poster_path}'), url('http://placehold.it/400x600/061535/1E8BC4?text=movie+poster')">
+                <h2>${movie.title} <span>${relase}</span></h2>
+                </li>`;
+                // console.log(movieElement);
+                container.insertAdjacentHTML('beforeend', movieElement);
+            })
 
-        msgSearch.innerHTML = `Showing results for: <strong>${searchValue}</strong>`
+            msgSearch.innerHTML = `Showing results for: <strong>${searchValue}</strong>`
+        } else {
+            msgSearch.innerHTML = `there are no resutls for: <strong>${searchValue}</strong>, try it again`
+        }
+
+
+
     }).catch(function(error) {
         msgSearch.innerHTML = `ERROR: <strong>${searchValue}</strong> <br/> ${error.message}`
 });
